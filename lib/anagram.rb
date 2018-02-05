5b4b25c600643f6848a77420dff3e4fbde733fc5
Kriti Rai <kriti.rai@gmail.com>
2018-02-05 21:37:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,14 +7,12 @@ class Anagram
   end
 
   def match(some_words)
-    some_words.each do |each_word|
-    	each_word.split("").sort.each do |word_split|
-    		if word_split == @word.split("").sort
-    			each_word
-    		else
-    			nil
-    		end
-    	end
+    some_words.map do |each_word|
+  		if each_word == @word.split("").sort
+  			each_word
+  		else
+  			nil
+  		end
     end
   end
 
