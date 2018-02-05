4b507f7470c44d4d614098c71557d30d1a42e54b
Kriti Rai <kriti.rai@gmail.com>
2018-02-05 20:16:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(some_words)
+  	
+    some_words.split.map do |each_word|
+    	each_word.split("").sort.each do |word_split| 
+    		
+    		if word_split == @word.split("").sort
+    			each_word
+    		else 
+    			nil
+    	
+    		end
+    		
+    	end
+    end
+  end
+ end
